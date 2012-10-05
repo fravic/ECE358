dda4c3769079c4c908b0fd3dd42a34d01875da6a
Fravic Fernando <fravicf@gmail.com>
2012-10-04 21:05:25 -0400

    Start simulation

--- Simulator.java
+++ Simulator.java
@@ -1,30 +1,49 @@
+/*
+  Simulator
+
+  Takes one argument: the number of ticks to perform.
+
+  Example usage: java Simulator 5000
+*/
+
 class Simulator {
-    public static void main(String[] args) {
-        /* Initialise important terms such as t_arrival = exponential r.v, # of pkts in queue = 0, 
-           t_departure = t_arrival ( this implies that first time departure will be called as soon as a 
-           packet arrives in the queue */
+
+    private int numPacketsInQueue;
+    private int tDeparture;
+    private int tArrival;
+
+    /* Initialise important terms such as t_arrival = exponential r.v, # of pkts in queue = 0,
+       t_departure = t_arrival ( this implies that first time departure will be called as soon as a 
+       packet arrives in the queue */
+    public void Simulator(int ticks) {
+        numPacketsInQueue = 0;
     }
 
-    private void startSimulation(int ticks) {
+    public void startSimulation(int ticks) {
         for (int t = 1; t <= ticks; t++) {
             arrival(t);
             departure(t);
         }
     }
 
+    /* Generate a packet as per the exponential distribution and insert the packet in the 
+       queue (an array or a linked list) */
     private void arrival(int t) {
-        /* Generate a packet as per the exponential distribution and insert the packet in the 
-           queue (an array or a linked list) */
     }
 
+    /* Check the queue for the packet, if head of the queue is empty, return 0 else if the 
+       queue is non-empty delete the packet from the queue after an elapse of the 
+       deterministic service time. */
     private void departure(int t) {
-        /* Check the queue for the packet, if head of the queue is empty, return 0 else if the 
-           queue is non-empty delete the packet from the queue after an elapse of the 
-           deterministic service time. */
     }
 
+    /* Calculate and display the results such as average number of packets in queue, 
+       average delay in queue and idle time for the server. */
     private void computePerformances() {
-        /* Calculate and display the results such as average number of packets in queue, 
-           average delay in queue and idle time for the server. */
+    }
+
+    public static void main(String[] args) {
+        Simulator s = new Simulator();
+        s.startSimulation(Integer.parseInt(args[0]));
     }
 }
